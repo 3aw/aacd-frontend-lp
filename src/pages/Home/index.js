163456d97ab635201e570d33f1d76517d5804897
Cayque Freitas <cayquefreitas@gmail.com>
2019-07-10 01:38:40 -0300

    Cleaning & Starting Header Component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,9 @@
 import React, { Component } from "react";
 import { colors, metrics } from "../../global/index";
-import Button from "../../components/Button";
-import GuestBox from "../../components/GuestBox";
-import SpeakerBox from "../../components/SpeakerBox";
-import TicketsBox from "../../components/TicketsBox";
 import { Container } from "./styles";
 
 export default class Home extends Component {
   render() {
-    return (
-      <Container>
-        <Button
-          bRadius={metrics.borderRadius.round}
-          backgroundColor={colors.white}
-          width={200}
-          bThickness={2}
-        >
-          Qualquer coisa
-        </Button>
-        <GuestBox />
-        <SpeakerBox />
-        <TicketsBox />
-      </Container>
-    );
+    return <Container />;
   }
 }
