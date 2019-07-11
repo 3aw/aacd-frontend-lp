b850a06e734ee9abe691b5e22248c4eabbe218fb
Cayque Freitas <cayquefreitas@gmail.com>
2019-07-11 13:36:33 -0300

    Form component finished

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,13 +1,57 @@
 import React, { Component } from "react";
-
-import { Container, Box, FormContainer } from "./styles";
+import Button from "../Button";
+import {
+  Container,
+  Box,
+  FormContainer,
+  TitleBar,
+  FormContent,
+  FormText,
+  InputElement
+} from "./styles";
 
 export default class Form extends Component {
   render() {
     return (
       <Container>
         <Box>
-          <FormContainer>teste</FormContainer>
+          <FormContainer>
+            <TitleBar />
+            <FormContent>
+              <FormText>Nome</FormText>
+              <InputElement type="text" />
+            </FormContent>
+            <FormContent>
+              <FormText>E-mail</FormText>
+              <InputElement type="text" />
+            </FormContent>
+            <FormContent>
+              <FormText>Celular</FormText>
+              <InputElement type="text" />
+            </FormContent>
+            <FormContent>
+              <FormText>Data de Nascimento</FormText>
+              <InputElement type="text" />
+            </FormContent>
+            <FormContent>
+              <FormText>RG</FormText>
+              <InputElement type="text" />
+            </FormContent>
+            <FormContent>
+              <FormText>Quantidade</FormText>
+              <InputElement type="text" />
+            </FormContent>
+            <FormContent>
+              <Button
+                width={"100%"}
+                bThickness={4}
+                backgroundColor={"#fff"}
+                bRadius={"none"}
+              >
+                Comprar!
+              </Button>
+            </FormContent>
+          </FormContainer>
         </Box>
         <Box />
       </Container>
--- src/components/Form/styles.js
+++ src/components/Form/styles.js
@@ -1,13 +1,15 @@
 import styled from "styled-components";
+import React from "react";
 import formbg from "../../images/backgroundForm.png";
 import girlbg from "../../images/formgirl.png";
+import { colors, metrics } from "../../global/index";
 
 export const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
 `;
-
+// item
 export const Box = styled.div`
   width: 50%;
   background: url(${formbg});
@@ -20,6 +22,81 @@ export const Box = styled.div`
     background-position: 50% 50%;
   }
 `;
+
+export const Cancel = styled.button`
+  background: none;
+  border: none;
+  color: ${colors.lightred};
+  font-size: 15px;
+  font-family: "Asap", sans-serif;
+  font-weight: 500;
+  letter-spacing: 2px;
+  text-transform: uppercase;
+  cursor: pointer;
+  display: block;
+  align-self: flex-end;
+`;
+
+export const FormTitle = styled.h2`
+  font-family: "Oswald", sans-serif;
+  font-size: ${metrics.fonts.xxlarge};
+  color: ${colors.red};
+  letter-spacing: 2px;
+  text-transform: uppercase;
+`;
+export const TitleBox = styled.div`
+  display: flex;
+  height: 85px;
+  justify-content: space-between;
+  position: relative;
+  &:after {
+    content: "";
+    display: block;
+    width: 260px;
+    height: 3px;
+    background: ${colors.red};
+    position: absolute;
+    bottom: 0;
+  }
+`;
+
+export const Item = styled.div`
+  align-self: ${props => props.alignSelf};
+`;
+
+export const TitleBar = () => (
+  <TitleBox>
+    <Item alignSelf={"center"}>
+      <FormTitle>Participe</FormTitle>
+    </Item>
+    <Item alignSelf={"flex-end"}>
+      <Cancel>Cancelar</Cancel>
+    </Item>
+  </TitleBox>
+);
+export const FormText = styled.p`
+  font-family: "Asap", sans-serif;
+  font-size: ${metrics.fonts.xmedium};
+  color: #000;
+  padding-bottom: 10px;
+  text-transform: uppercase;
+  font-weight: 500;
+`;
+export const InputElement = styled.input`
+  width: 100%;
+  padding: 15px 20px 10px;
+  border: solid 2px #6f6f6f;
+  font-family: "Asap", sans-serif;
+  font-size: ${metrics.fonts.xmedium};
+  :focus {
+    border: solid 2px ${colors.lightred};
+    color: ${colors.lightred};
+  }
+`;
+export const FormContent = styled.div`
+  width: 100%;
+  padding: 20px 0 10px;
+`; // form item
 
 export const FormContainer = styled.div`
   height: 100vh;
