0c530ed86930dc7b93afa8d5a48705d1e693c9c0
Samuel Levy <samuellevy@meshproducoes.com.br>
2019-07-24 14:44:22 -0300

    fix button position

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,18 +1,7 @@
 import React, { Component } from "react";
 import { connect, useDispatch } from "react-redux";
 import Button from "../Button";
-import {
-  Container,
-  Box,
-  FormContainer,
-  TitleBar,
-  FormContent,
-  FormText,
-  InputElement,
-  ParticipantBox,
-  TitleForm,
-  SubscribersContainer
-} from "./styles";
+import { Container, Box, FormContainer, TitleBar, FormContent, FormText, InputElement, ParticipantBox, TitleForm, SubscribersContainer } from "./styles";
 import * as formActions from "../../store/reducers/actions/form";
 
 export class Form extends Component {
@@ -104,54 +93,26 @@ export class Form extends Component {
             <TitleForm>Dados do comprador</TitleForm>
             <FormContent>
               <FormText>Nome</FormText>
-              <InputElement
-                onChange={this.handleChange}
-                type="text"
-                name="name"
-              />
+              <InputElement onChange={this.handleChange} type="text" name="name" />
             </FormContent>
             <FormContent>
               <FormText>E-mail</FormText>
-              <InputElement
-                onChange={this.handleChange}
-                type="text"
-                name="email"
-              />
+              <InputElement onChange={this.handleChange} type="text" name="email" />
             </FormContent>
             <FormContent>
               <FormText>Celular</FormText>
-              <InputElement
-                onChange={this.handleChange}
-                type="text"
-                name="cellphone"
-              />
+              <InputElement onChange={this.handleChange} type="text" name="cellphone" />
             </FormContent>
             <FormContent>
               <FormText>Data de Nascimento</FormText>
-              <InputElement
-                onChange={this.handleChange}
-                type="text"
-                name="birthdate"
-              />
+              <InputElement onChange={this.handleChange} type="text" name="birthdate" />
             </FormContent>
             <FormContent>
               <FormText>RG</FormText>
-              <InputElement
-                onChange={this.handleChange}
-                type="text"
-                name="id"
-              />
+              <InputElement onChange={this.handleChange} type="text" name="id" />
             </FormContent>
             <FormContent>
-              <Button
-                onClick={this.newSubscriber}
-                width={40}
-                height={15}
-                backgroundColor={"#fff"}
-                bRadius={"8px"}
-                bThickness={2}
-                center={true}
-              >
+              <Button onClick={this.newSubscriber} width={40} height={15} backgroundColor={"#fff"} bRadius={"8px"} bThickness={2}>
                 Adicionar participante
               </Button>
             </FormContent>
@@ -162,19 +123,11 @@ export class Form extends Component {
                   <ParticipantBox key={index}>
                     <FormContent>
                       <FormText>Nome</FormText>
-                      <InputElement
-                        onChange={e => this.handleChangeSubscriber(e, index)}
-                        type="text"
-                        name="name"
-                      />
+                      <InputElement onChange={e => this.handleChangeSubscriber(e, index)} type="text" name="name" />
                     </FormContent>
                     <FormContent>
                       <FormText>E-mail</FormText>
-                      <InputElement
-                        onChange={e => this.handleChangeSubscriber(e, index)}
-                        type="text"
-                        name="email"
-                      />
+                      <InputElement onChange={e => this.handleChangeSubscriber(e, index)} type="text" name="email" />
                     </FormContent>
                   </ParticipantBox>
                 </>
@@ -183,13 +136,7 @@ export class Form extends Component {
 
             {this.state.form}
             <FormContent>
-              <Button
-                width={"100%"}
-                bThickness={2}
-                height={20}
-                backgroundColor={"#fff"}
-                bRadius={"32px"}
-              >
+              <Button width={"100%"} bThickness={2} height={20} backgroundColor={"#fff"} bRadius={"32px"}>
                 Comprar!
               </Button>
             </FormContent>
