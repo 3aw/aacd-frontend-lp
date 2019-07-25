87e9259af6b63fd634d2efb6f5520f136031f1c4
Cayque Freitas <cayquefreitas@gmail.com>
2019-07-25 19:36:19 -0300

    Info added

--- src/store/reducers/speakers.js
+++ src/store/reducers/speakers.js
@@ -1,85 +1,103 @@
 import Speaker from "../../images/Speakers/dr_claudio.png";
+import Speaker2 from "../../images/Speakers/bernadinho.png";
+import Speaker3 from "../../images/Speakers/justus.png";
+import Speaker4 from "../../images/Speakers/coen.png";
+import Speaker5 from "../../images/Speakers/anatheresa.png";
+import Speaker6 from "../../images/Speakers/gilgiardelli.png";
+import Speaker7 from "../../images/Speakers/annachaia.png";
+import Speaker8 from "../../images/Speakers/luizahelena.png";
+import Speaker9 from "../../images/Speakers/paulostorani.png";
+import Speaker10 from "../../images/Speakers/marcelokheirallah.png";
 import letterI from "../../images/Backgrounds/letter_t.png";
+import letterM from "../../images/Backgrounds/letter_m.png";
+import letterP from "../../images/Backgrounds/letter_p.png";
+import letterA from "../../images/Backgrounds/letter_a.png";
+import letterC from "../../images/Backgrounds/letter_c.png";
+import letterT from "../../images/Backgrounds/letter_t.png";
+import letterN from "../../images/Backgrounds/letter_n.png";
+import letterD from "../../images/Backgrounds/letter_d.png";
+import letterO from "../../images/Backgrounds/letter_o.png";
 const INITIAL_STATE = [
   {
     id: 1,
     background: letterI,
     image: Speaker,
-    theme: "Motivação",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    theme: "Inovação",
+    name: "Dr. Claudio Lottenberg",
+    ocupation:
+      "Presidente Do Conselho De Administração Do Hospital Albert Einstein"
   },
   {
     id: 2,
-    background: letterI,
-    image: Speaker,
+    background: letterM,
+    image: Speaker2,
     theme: "Maestria",
     name: "Bernadinho",
     ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
   },
   {
     id: 3,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterP,
+    image: Speaker3,
+    theme: "Produtividade",
+    name: "Roberto Justus",
+    ocupation: "Empresário e apresentador"
   },
   {
     id: 4,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterA,
+    image: Speaker4,
+    theme: "Amor",
+    name: "Monja Coen",
+    ocupation: "Monja Coen"
   },
   {
     id: 5,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterC,
+    image: Speaker5,
+    theme: "Confiança",
+    name: "Ana Theresa Borsari",
+    ocupation: "Presidente da peugeot brasil e conselheira da vivo"
   },
   {
     id: 6,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterT,
+    image: Speaker6,
+    theme: "Transformação",
+    name: "Gil Giardelli",
+    ocupation: "Professor Do Mit, Colunista Bandnews E Ref. Em Inovação Digital"
   },
   {
     id: 7,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterA,
+    image: Speaker7,
+    theme: "Adaptabilidade",
+    name: "Anna Chaia",
+    ocupation: "Ceo da samsonite mercosul e escritora"
   },
   {
     id: 8,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterN,
+    image: Speaker8,
+    theme: "Negócio",
+    name: "Luiza Helena Trajano",
+    ocupation: "Presidenta do conselho de administração do magazine luiza"
   },
   {
     id: 9,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterD,
+    image: Speaker9,
+    theme: "Determinação e Dedicação",
+    name: "Paulo Storani",
+    ocupation: "Ex-comandante Do bope"
   },
   {
     id: 10,
-    background: letterI,
-    image: Speaker,
-    theme: "Maestria",
-    name: "Bernadinho",
-    ocupation: "Técnico da Seleção Brasileira de voleibol e campeão olímpico"
+    background: letterO,
+    image: Speaker10,
+    theme: "Ong oportunidade aacd",
+    name: "Marcelo Kheirallah",
+    ocupation: "Pres. Voluntário do conselho de admin. Da aacd"
   }
 ];
 
